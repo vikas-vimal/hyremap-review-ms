fce70039a337186f292db12d8ca1a7334181190d
Vikki <vikas@hood.live>
2024-04-12 12:51:42 +0530

    Blank spring boot application

---
+++ src/main/java/com/designfox/hyremapreviewms/HyremapReviewMsApplication.java
@@ -0,0 +1,13 @@
+package com.designfox.hyremapreviewms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyremapReviewMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyremapReviewMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/designfox/hyremapreviewms/HyremapReviewMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.designfox.hyremapreviewms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyremapReviewMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
